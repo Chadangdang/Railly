d4c804c8dfbbc66484df4077ad4bda55868125c6
chadang <chadang.phu@gmail.com>
2025-10-28 03:37:01 +0700

    head foot fix place

--- Page/user/Aboutus/aboutus.js
+++ Page/user/Aboutus/aboutus.js
@@ -2,48 +2,13 @@
   'use strict';
 
   function initAboutPage() {
-    var session = window.userSession;
-    if (!session) {
+    if (!window.userSession) {
       return;
     }
 
-    var context = session.getUserContext();
-    session.applyUserContextToLinks('.about-header .main-nav a, .about-header .brand, .about-header .user-profile-link');
-
-    var loginLink = document.querySelector('.about-header .login-link');
-    var signupBtn = document.querySelector('.about-header .signup-btn');
-    var userProfileLink = document.querySelector('.about-header .user-profile-link');
-
-    if (context && context.username && context.id) {
-      if (loginLink) {
-        loginLink.classList.add('is-hidden');
-      }
-
-      if (signupBtn) {
-        signupBtn.classList.add('is-hidden');
-      }
-
-      if (userProfileLink) {
-        userProfileLink.classList.remove('is-hidden');
-        session.applyUserContextToLinks('.about-header .user-profile-link');
-      }
-    } else {
-      if (loginLink) {
-        loginLink.classList.remove('is-hidden');
-        loginLink.removeAttribute('id');
-        loginLink.classList.remove('logout-link');
-        loginLink.href = '../Login/Login.html';
-      }
-
-      if (signupBtn) {
-        signupBtn.classList.remove('is-hidden', 'user-pill');
-        signupBtn.href = '../Signup/Signup.html';
-      }
-
-      if (userProfileLink) {
-        userProfileLink.classList.add('is-hidden');
-      }
-    }
+    window.userSession.applyUserContextToLinks(
+      '.site-header .main-nav a, .site-header .brand, .site-header .user-profile-link'
+    );
   }
 
   if (window.__layoutReady) {
